5cc1bbdddd2d982c1c42392cdb8c3a1bec14ab61
productuidev <productuidev@gmail.com>
2022-05-17 15:23:56 +0900

    useParams (custom hooks)

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,4 +1,10 @@
+import { useParams } from "react-router-dom"; // custom hooks
+
+// pathVariable = id
 const Diary = () => {
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div>
       <h1>Diary</h1>
