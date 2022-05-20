2b07c7ec8a84526aa69ab220b927da0797499dd9
productuidev <productuidev@gmail.com>
2022-05-21 00:34:20 +0900

    css styling DiaryItem

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,8 +1,11 @@
 const DiaryItem = ({ id, emotion, content, date }) => {
   return (
     <div className="DiaryItem">
-      <div className="emotionImgWrapper">
-        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
+      <div className={[
+        "emotionImgWrapper",
+        `emotionImgWrapper${emotion}`
+      ].join(" ")}>
+        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt={`감정점수 ${emotion}점`} />
       </div>
       <div></div>
       <div></div>
