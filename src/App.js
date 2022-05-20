73c9c82bd6bdbd1ae93fbba7d1c591416a8e0b63
productuidev <productuidev@gmail.com>
2022-05-20 23:13:25 +0900

    dummyData

--- src/App.js
+++ src/App.js
@@ -38,17 +38,19 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-// 일기 더미데이터
+// 일기 더미데이터 (시간순)
 const dummyData = [
-  {
-    id:1, emotion:1, content:"오늘의일기 1번", date:1653055859902,
-  },
+  { id:1, emotion:1, content:"오늘의일기 1번", date:1653055859902, },
+  { id:2, emotion:2, content:"오늘의일기 2번", date:1653055859903, },
+  { id:3, emotion:3, content:"오늘의일기 3번", date:1653055859904, },
+  { id:4, emotion:4, content:"오늘의일기 4번", date:1653055859905, },
+  { id:5, emotion:5, content:"오늘의일기 5번", date:1653055859906, },
 ]
 
 function App() {
   // data의 기본 state는 []
   const [data, dispatch] = useReducer(reducer, []);
-  console.log(new Date().getTime(dummyData)); // 현재시간
+  //console.log(new Date().getTime(dummyData)); // 현재시간
 
   // 일기 id로 사용
   const dataId = useRef(0);
