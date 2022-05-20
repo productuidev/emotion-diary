d0b5ff7bf0c746879aa8a931e020a25c49ebb148
productuidev <productuidev@gmail.com>
2022-05-21 00:24:05 +0900

    emotionImgWrapper

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,7 +1,11 @@
-const DiaryItem = () => {
+const DiaryItem = ({ id, emotion, content, date }) => {
   return (
     <div className="DiaryItem">
-      
+      <div className="emotionImgWrapper">
+        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
+      </div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
