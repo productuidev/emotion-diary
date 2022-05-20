e17d16dbdaab43fb994e03f02fb50096b1fa7138
productuidev <productuidev@gmail.com>
2022-05-21 00:40:50 +0900

    DiaryItem content (maximum to 25)

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,4 +1,9 @@
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+
   return (
     <div className="DiaryItem">
       <div className={[
@@ -7,8 +12,13 @@ const DiaryItem = ({ id, emotion, content, date }) => {
       ].join(" ")}>
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt={`감정점수 ${emotion}점`} />
       </div>
-      <div></div>
-      <div></div>
+      <div className="infoWrapper">
+        <div className="DiaryDate">{strDate}</div>
+        <div className="DiaryContentPreview">{content.slice(0, 25)}</div>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 };
