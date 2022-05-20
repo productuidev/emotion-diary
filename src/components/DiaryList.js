4c00ab5229d86360de286f8e6d891d2531d95b9c
productuidev <productuidev@gmail.com>
2022-05-20 23:48:50 +0900

    fiterOptionList

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const sortOptionList = [
-  { value: "latest", name:"최신순" },
-  { value: "oldest", name:"오래된 순" },
-]
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const filterOptionList = [
+  { value: "all", name: "전부다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안좋은 감정만" },
+];
 
 // Memu
 const ControlMenu = ({value, onChange, optionList}) => {
@@ -20,6 +26,8 @@ const ControlMenu = ({value, onChange, optionList}) => {
 const DiaryList = ({diaryList}) => {
   // sort
   const [sortType, setSortType] = useState("latest");
+  // filter
+  const [filter, setFilter] = useState("all");
 
   const getProcessedDiaryList = () => {
     const compare = (a, b) => {
