4ae9b75918c67ed4540e07fb7e313eedd931eed8
productuidev <productuidev@gmail.com>
2022-05-24 07:45:12 +0900

    add header in New

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+
 const New = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>New</h1>
-      <p>이곳은 새 페이지 입니다.</p>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={<MyButton text={"< 뒤로가기"} onClick={()=>navigate(-1)} />} />
     </div>
   );
 }
