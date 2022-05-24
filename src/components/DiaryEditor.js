8a061153214004ecaae6107cfad73066f0525611
productuidev <productuidev@gmail.com>
2022-05-25 07:59:11 +0900

    create DiaryEditor.js (move New.js contents)

---
+++ src/components/DiaryEditor.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MyHeader from "./MyHeader";
+import MyButton from "./MyButton";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
+const DiaryEditor = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
+
+  const navigate = useNavigate();
+  return (
+    <div>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={<MyButton text={"< 뒤로가기"} onClick={()=>navigate(-1)} />}
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="inputBox">
+            <input
+              className="inputDate"
+              type="date"
+              value={date}
+              onChange={(e)=>setDate(e.target.value)} />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,37 +1,11 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-import MyHeader from "./../components/MyHeader";
-import MyButton from "./../components/MyButton";
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
-  const [date, setDate] = useState(getStringDate(new Date()));
-
-  const navigate = useNavigate();
   return (
     <div>
-      <MyHeader
-        headText={"새 일기쓰기"}
-        leftChild={<MyButton text={"< 뒤로가기"} onClick={()=>navigate(-1)} />}
-      />
-      <div>
-        <section>
-          <h4>오늘은 언제인가요?</h4>
-          <div className="inputBox">
-            <input
-              className="inputDate"
-              type="date"
-              value={date}
-              onChange={(e)=>setDate(e.target.value)} />
-          </div>
-        </section>
-      </div>
+      <DiaryEditor />
     </div>
   );
-}
+};
 
 export default New;
