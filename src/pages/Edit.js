183281e8e0b25c74d5941b45a44e127a96e30e00
productuidev <productuidev@gmail.com>
2022-05-26 07:52:15 +0900

    Edit bakup

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,24 +1,11 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Edit = () => {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  
-  const id = searchParams.get("id");
-  console.log("id :", id);
-
-  const mode = searchParams.get("mode");
-  console.log("mode :", mode);
-
 
   return (
     <div>
-      <h1>Edit</h1>
-      <p>이곳은 수정페이지 입니다.</p>
-      <button onClick={()=>setSearchParams({who: "winterlood"})}>QS Change</button>
-      <button onClick={()=>{navigate("/home")}}>Home</button>
-      <button onClick={()=>{navigate(-1)}}>Back</button>
-      <p>&nbsp;</p>
+      
     </div>
   );
 }
