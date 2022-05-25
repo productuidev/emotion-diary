c739e1dec4b4f1a7b62391760407d9939893fcd4
productuidev <productuidev@gmail.com>
2022-05-26 07:57:08 +0900

    path id useParams

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Edit = () => {
   const navigate = useNavigate();
+  const {id} = useParams(); // 현재 전달받은 id
+  console.log(id);
 
   return (
     <div>
