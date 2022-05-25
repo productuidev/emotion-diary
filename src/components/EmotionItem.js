0fcce7d46e1039cb39533e0ba3e0cf35755c90bb
productuidev <productuidev@gmail.com>
2022-05-25 23:30:58 +0900

    css styling EmotionItem

--- src/components/EmotionItem.js
+++ src/components/EmotionItem.js
@@ -1,6 +1,6 @@
 const EmotionItem = ({ emotion_id, emotion_img, emotion_descript }) => {
   return (
-    <div>
+    <div className="EmotionItem">
       <img src={emotion_img} alt={emotion_descript} />
       <span>{emotion_descript}</span>
     </div>
