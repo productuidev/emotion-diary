728461165f2b81439fa44f0a2d2c69f63515c966
productuidev <productuidev@gmail.com>
2022-05-31 00:15:53 +0900

    delete useEffect

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DiaryItem from "./DiaryItem";
 import MyButton from "./MyButton";
