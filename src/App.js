24b6071e94750848fbe5b1547b50e4ef0bdeb1bb
productuidev <productuidev@gmail.com>
2022-05-31 01:03:16 +0900

    if condition diaryList

--- src/App.js
+++ src/App.js
@@ -66,7 +66,6 @@ function App() {
       const diaryList = JSON.parse(localData).sort(
         (a,b) => parseInt(b.id) - parseInt(a.id)
       );
-
       if(diaryList.length >= 1) {
         dataId.current = parseInt(diaryList[0].id) + 1
         dispatch({type:"INIT", data:diaryList});
