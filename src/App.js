cfd7158a8150592c0ba57827de8a55d83ff4aa2a
productuidev <productuidev@gmail.com>
2022-05-31 23:35:18 +0900

    localData.length

--- src/App.js
+++ src/App.js
@@ -45,14 +45,11 @@ export const DiaryDispatchContext = React.createContext();
 function App() {
   useEffect(()=>{
     const localData = localStorage.getItem("diary");
-    if(localData) {
-      const diaryList = JSON.parse(localData).sort(
-        (a,b) => parseInt(b.id) - parseInt(a.id)
-      );
-      if(diaryList.length >= 1) {
-        dataId.current = parseInt(diaryList[0].id)+1;
-        dispatch({type:"INIT", data:diaryList});
-      }
+    if(localData.length > 2) {
+      const diaryList = JSON.parse(localData).sort((a,b) => parseInt(b.id) - parseInt(a.id));
+      dataId.current = parseInt(diaryList[0].id)+1;
+      
+      dispatch({type:"INIT", data:diaryList});
     }
   }, []);
 
